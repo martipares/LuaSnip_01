a411eb0e693ac112862b2f50c602acbf597ed488
martipares <martiparesmath@gmail.com>
2025-03-25 17:43:51 +0100

    add color mod to theme

--- lazyvim_config/plugins_2025_03_23/theme.lua
+++
@@ -1,9 +0,0 @@
-return {
-    { "folke/tokyonight.nvim" },
-    {
-        "folke/tokyonight.nvim",
-        lazy = true,
-        -- theme tokyonight.nvim moon, storm, night, day
-        opts = { style = "moon" },
-    }
-}
